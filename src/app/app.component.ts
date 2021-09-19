51e6e5a3fa72d988f86748d0817b56c7c02d804f
Asher Indursky <asheri@proftit.com>
2021-09-19 16:18:32 +0300

    Zone.js triggered Change Detection also from pure js event as well as 3d libs events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,10 @@ import {AfterViewInit, Component} from '@angular/core';
           <h1>
               Welcome to {{title}}!
           </h1>
-          <div (click)="nope()">
-              <button (click)="nope()">click</button>
+          <div (click)="nope($event)">
+              <button (click)="nope($event)">Click</button>
           </div>
+          <button class="trigger-cd-from-zone">Trigger CD on click from Zone.js</button>
           <app-child></app-child>
       </div>
       <router-outlet></router-outlet>
@@ -20,15 +21,23 @@ import {AfterViewInit, Component} from '@angular/core';
 export class AppComponent implements AfterViewInit {
   title = 'ng12app';
 
-  /**
-   * direct DOM Manipulation is bad practice because `app-child` Component
-   * still exist in the Angular components tree and cause Change Detection
-   */
+
   ngAfterViewInit(): void {
+    /**
+     * direct DOM Manipulation is bad practice because `app-child` Component
+     * still exist in the Angular components tree and cause Change Detection
+     */
     const root: HTMLElement | null = document.querySelector('app-child');
     root?.parentNode?.removeChild(root);
+
+    /**
+     * Zone.js triggered Change Detection also from pure js event as well as 3d libs events.
+     */
+    const button: HTMLElement | null = document.querySelector('.trigger-cd-from-zone');
+    button?.addEventListener('click', (e) => this.nope(e));
   }
 
-  nope() {
+  nope(e: MouseEvent) {
+    console.log('nope', e?.target);
   }
 }
